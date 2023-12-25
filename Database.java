47f8b2f4903c84434e403317c881c7dd08f77ca4
2022236 <146198723+2022236@users.noreply.github.com>
2023-12-25 17:13:26 +0000

    Update Database.java
    Adding comments however not much changed

--- Database.java
+++ Database.java
@@ -1,18 +1,22 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ca2calculationtax;
+package OOCTaxCalculater;
 
 /**
  *
- * @author lizam
+ * @author Lizandra Matos 2022336 and Taciana Oliveira 2022404
  */
+// an abstract class representing a Database
 public abstract class Database {
+   // Constants defining database connection parameters
    protected final static String dbBaseURL = "jdbc:mysql://localhost";
-   protected final static String USER = "ooc2023";
+   protected final static String USER = "OOC2023";
    protected final static String PASSWORD = "ooc2023";
    protected final static String dbName = "mydb";
    protected final static String tableName = "User";
    protected final static String db_URL = dbBaseURL + "/" + dbName;
+   
+   
 }
